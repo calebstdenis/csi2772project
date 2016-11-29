a254785ffe169f62173bf7e3efe5892c3deea9b6
caleb-st <caleb_stdenis@hotmail.com>
2016-11-29 09:54:11 -0500

    private derivation for Deck

--- JeuDeCartes/JeuDeCartes/Deck.h
+++ JeuDeCartes/JeuDeCartes/Deck.h
@@ -5,7 +5,8 @@
 
 class CardFactory;
 
-class Deck : public std::vector<Card*> {
+//Derivation is private to prevent insertion of Cards. Useful members are made public with "using" however.
+class Deck : private std::vector<Card*> {
 	friend class CardFactory;
 	Deck(); //no point in defining a destructor as Deck does not maintain any special state on the Heap. Vector does not have a virtual desctructor besides.
 	template <class C> inline Deck& addCards(int n) { //Used by CardFactory to create cards while staying DRY
@@ -17,5 +18,9 @@ public:
 	Deck(const std::istream&, CardFactory*);
 	Card* draw();
 	friend std::ostream& operator<<(std::ostream&, const Deck&);
+	using vector<Card*>::size;
+	using vector<Card*>::begin;
+	using vector<Card*>::end;
+	using vector<Card*>::empty;
 };
 #endif
