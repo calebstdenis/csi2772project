0836a9393380522fd2dbffe54e970b962646d614
Daviiiiid <Daviiiiid@DAVIDBONSANT_JR>
2016-12-09 14:54:44 -0500

    commencé Hand.cpp peut-être qu'il faut que j'utilise autre chose qu'un deque pour operator[]

--- JeuDeCartes/JeuDeCartes/Hand.h
+++ JeuDeCartes/JeuDeCartes/Hand.h
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <deque>
 
 class Card;
 class CardFactory;
 
 class Hand
 {
+	std::deque<Card*> main;
 public:
 	Hand(const std::istream&, CardFactory*);
 	Hand& operator+=(Card*);
