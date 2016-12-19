06ced70875f50acb053c00b3f20a147d559ebe3f
Daviiiiid <Daviiiiid@DAVIDBONSANT_JR>
2016-12-19 16:07:53 -0500

    terminé player

--- JeuDeCartes/JeuDeCartes/Player.cpp
+++ JeuDeCartes/JeuDeCartes/Player.cpp
@@ -10,7 +10,40 @@ Player::Player(std::string& nom)
 
 Player::Player(std::istream & is, CardFactory* cf)
 {
+	if (!(is >> nom))
+		throw corrupt_game_file_exception();
+	if (!(is >> numCoins))
+		throw corrupt_game_file_exception();
+	if (!(is >> maxNumChain))
+		throw corrupt_game_file_exception();
+	main = *new Hand(is,cf);
 
+	ChainBase* c;
+	std::string s;
+	while (is >> s)
+	{
+		if (s == Quartz::name)
+			c = new Chain<Quartz>(is, cf);
+		else if (s == Hematite::name)
+			c = new Chain<Hematite>(is, cf);
+		else if (s == Quartz::name)
+			c = new Chain<Obsidian>(is, cf);
+		else if (s == Malachite::name)
+			c = new Chain<Malachite>(is, cf);
+		else if (s == Turquoise::name)
+			c = new Chain<Turquoise>(is, cf);
+		else if (s == Ruby::name)
+			c = new Chain<Ruby>(is, cf);
+		else if (s == Amethyst::name)
+			c = new Chain<Amethyst>(is, cf);
+		else if (s == Emerald::name)
+			c = new Chain<Emerald>(is, cf);
+		else
+			throw corrupt_game_file_exception();
+		chain.push_back(*c);
+	}
+	if (getNumChains() > maxNumChain)
+		throw corrupt_game_file_exception();
 }
 
 std::string Player::getName() const
@@ -42,7 +75,7 @@ int Player::getNumChains()
 	return chain.size;
 }
 
-Chain<Card*>& Player::operator[](int i)
+Chain<Card>& Player::operator[](int i)
 {
 	return chain[i];
 }
@@ -52,7 +85,7 @@ void Player::buyThirdChain()
 	if (maxNumChain == 2)
 	{
 		if (numCoins < 2)
-			throw new NotEnoughCoinsException();
+			throw NotEnoughCoinsException(numCoins);
 		else
 		{
 			numCoins -= 2;
@@ -65,7 +98,7 @@ void Player::printHand(std::ostream& os, bool premier)
 {
 	if (premier)
 	{
-		main.top()->print();
+		main.top()->print(os);
 	}
 	else
 	{
@@ -76,9 +109,21 @@ void Player::printHand(std::ostream& os, bool premier)
 std::ostream& operator<<(std::ostream& os, const Player & player)
 {
 	os << player.nom << std::setw(IOUtil::COLUMN_WIDTH) << player.numCoins << " Coins" << endl;
-	for (Chain<Card*> c : player.chain)
+	for (Chain<Card> c : player.chain)
 	{
 		os << c;
+	}
+}
+
+void Player::print(std::ostream& os)
+{
+	os << nom << endl;
+	os << numCoins << endl;
+	os << maxNumChain << endl;
+	os << main << endl;
+	for (Chain<Card> c : chain)
+	{
+		os << c << endl;
 	}
 }
 
